58fc3b1a4a4363381b3f2b3edd75e44b6dc8c04c
HeTong <hetong12345@gmail.com>
2018-03-19 13:43:37 +0800

    learn mooc

---
+++ egg/throwEgg.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+)
+
+var floorSum int
+var floorbest int
+var nextFloor int = 1
+var operaSum int = 0
+
+type egg struct {
+	num   int
+	statu bool
+}
+
+var egg1 = &egg{1, false}
+var egg2 = &egg{2, false}
+
+func main() {
+	floorSum = 10000
+	floorbest = 7491
+	fmt.Println("初始化完成，楼层高度为：", floorSum, "最佳楼层为：", floorbest)
+	fmt.Println("----------------开始方法一------------------")
+	fmt.Println("经计算最佳楼层是", algo1()-1)
+	fmt.Println("计算花费步数是：", operaSum)
+	fmt.Println("----------------开始方法二------------------")
+	fmt.Println("经计算最佳楼层是", algo2()-1)
+	fmt.Println("计算花费步数是：", operaSum)
+}
+
+func algo1() int { //解决算法，提供下层楼数
+	nextFloor = 1
+	operaSum = 0
+	for {
+		egg1.statu = isBroken(nextFloor)
+		if egg1.statu {
+			break
+		}
+		nextFloor++
+		operaSum++
+	}
+	return nextFloor
+}
+
+func algo2() int {
+	nextFloor = 1
+	operaSum = 0
+
+	var urange int = 500
+
+	for {
+		egg1.statu = isBroken(nextFloor)
+		if egg1.statu {
+			break
+		}
+		nextFloor += urange
+		operaSum++
+	}
+	nextFloor -= urange
+	for {
+		egg2.statu = isBroken(nextFloor)
+		if egg2.statu {
+			break
+		}
+		nextFloor++
+		operaSum++
+	}
+	operaSum++
+	return nextFloor
+}
+
+func isBroken(floor int) bool { //判断结果
+	if floor <= floorbest {
+		return false
+	} else {
+		return true
+	}
+}
---
+++ for.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+import "os"
+import "os/signal"
+import "syscall"
+
+func main() {
+	go SignalProc()
+
+	done := make(chan bool, 1)
+	for {
+		select {
+		case <-done:
+			break
+		}
+
+	}
+	fmt.Println("exit")
+
+}
+
+func SignalProc() {
+	sigs:= make(chan os.Signal)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGHUP,os.Interrupt)
+
+	for {
+		msg := <-sigs
+		fmt.Println("Recevied signal:", msg)
+
+		switch msg {
+		default:
+			fmt.Println("get sig=%v\n", msg)
+		case syscall.SIGHUP:
+			fmt.Println("get sighup\n")
+		case syscall.SIGUSR1:
+			fmt.Println("SIGUSR1 test")
+		case syscall.SIGUSR2:
+			fmt.Println("SIGUSR2 test")
+		}
+	}
+}

5219bac08fbfcc4fcff104992bf7a3f753c88eb5
renketong <hetong12345@gmail.com>
2018-08-07 18:07:57 +0800

    winner coder

---
+++ acm/lintcode/removeListElements.go
@@ -0,0 +1,10 @@
+package lintcode
+func RemoveElements(head *ListNode, val int) *ListNode {
+	// write your code here
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
--- acm/run.go
+++ acm/run.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"./data"
+	"./lintcode"
 	"fmt"
 )
 
@@ -10,17 +11,28 @@ type student struct {
 	score int
 }
 
+
 func main() {
 	//ls := data.CreateListStack()
 	//as:=data.CreateArrayStack(10)
 	//
-	op:=100000
+	op := 100000
 
 	listq := data.CreateListQueue()
 	loopq := data.CreateLoopQueue(10)
 	aq := data.CreateArrayQueue(10)
 
-	fmt.Println(data.TestQueue(listq,op))
-	fmt.Println(data.TestQueue(loopq,op))
-	fmt.Println(data.TestQueue(aq,op))
+	fmt.Println(data.TestQueue(listq, op))
+	fmt.Println(data.TestQueue(loopq, op))
+	fmt.Println(data.TestQueue(aq, op))
+
+	head:=lintcode.ListNode{
+		1,
+		&lintcode.ListNode{
+			2,
+			&lintcode.ListNode{},
+		},
+	}
+	lintcode.RemoveElements(&head,4)
 }
+

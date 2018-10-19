e36eabc2e4141fbb68d05d48f02f9ccceeb42d66
renketong <hetong12345@gmail.com>
2018-10-19 18:09:48 +0800

    go的几种设计模式实践

--- main_test.go
+++ main_test.go
@@ -1,20 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/hetong12345/learn/single"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
-	leshi := &single.LeshiGlasses{FirstCost: 100}
-	var glassesbuilder single.Builder
-	glassesbuilder = &single.ShanWeiBuilder{}
-	glasses := leshi.GetGlasses(glassesbuilder)
-	fmt.Println("glasses's price is: ", glasses.Price, " glasses from :", glasses.From)
-	glassesbuilder = &single.ShenZhenBuilder{}
-	glasses = leshi.GetGlasses(glassesbuilder)
-	fmt.Println("glasses's price is: ", glasses.Price, " glasses from :", glasses.From)
-
-	return
+	company := &single.BaseCompany{}
+	developingCompany := &single.DevelopingCompany{NewCompany: company}
+	developingCompany.Showing()
+	bigCompany := &single.NewBigCompany{NewCompany: developingCompany}
+	bigCompany.Showing()
 }
